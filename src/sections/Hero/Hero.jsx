93a489a7f4b9985bf49edaec8b30802b8cc8695e
pedrogomes107 <pedromcgomes95@gmail.com>
2024-12-23 04:10:53 +0000

    Load my profile picture

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/Design uten navn.png";
+import heroImg from "../../assets/chatGTPme.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
 import githubLight from "../../assets/github-light.svg";
