bba7d18a66a137cccc91e1f094025e23ac7adaab
pedrogomes107 <pedromcgomes95@gmail.com>
2025-01-10 00:10:32 +0000

    Add new 2 Projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,6 +2,8 @@ import styles from "./ProjectsStyles.module.css";
 import twitterClone from "../../assets/logotipo-twitter.png";
 import nikeWebsite from "../../assets/logotipo-nike.png";
 import Promptopia from "../../assets/logotipo-promptopia.png";
+import tinderClone from "../../assets/logotipo-tinder.jpg";
+import chatApp from "../../assets/logotipo-chatApp.png";
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -16,16 +18,28 @@ function Projects() {
           p="App with the most basic features of Twitter, like posting tweets, following users, and liking tweets."
         />
         <ProjectCard
+          src={Promptopia}
+          link="https://my-nextjs-project-promptopia.vercel.app/"
+          h3="Promptopia"
+          p="A platform to discover, create and share AI-Powered Prompts. Built with Next.js and Tailwind CSS."
+        />
+        <ProjectCard
+          src={tinderClone}
+          link="https://mern-clone-tinder.onrender.com"
+          h3="Tinder Clone"
+          p="App with the most basic features of Tinder, like swiping, matching, and chatting. Built with MERN stack."
+        />
+        <ProjectCard
           src={nikeWebsite}
           link="https://tailwind-nike-website.onrender.com/"
           h3="Nike Website"
           p="Mock frontpage of Nike's official website, with the most basic features. All built with Tailwind CSS."
         />
         <ProjectCard
-          src={Promptopia}
-          link="https://my-nextjs-project-promptopia.vercel.app/"
-          h3="Promptopia"
-          p="A platform to discover, create and share AI-Powered Prompts. Built with Next.js and Tailwind CSS."
+          src={chatApp}
+          link="https://mern-chat-app-17im.onrender.com"
+          h3="Realtime Chat App"
+          p="Realtime chat app with rooms and private messages. Built with MERN stack, Socket.io and with Tailwind CSS."
         />
       </div>
     </section>
